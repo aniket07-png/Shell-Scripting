4786b7c04d9f8509a2a8eee6fdf04917dc8d9430
aniket07-png <aniket9369020880@gmail.com>
2025-12-15 12:41:37 +0530

    Add first shell script

---
+++ 01_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#This is single line comment
+
+<<comment
+This
+is
+multi
+line comments
+comment
+
+name="Aniket"
+age=20
+
+echo "My name is $name and age is $age"
+
+name="Harry"
+
+echo "His name is $name"
+
